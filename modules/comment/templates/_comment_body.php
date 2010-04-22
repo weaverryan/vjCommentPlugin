4474d43279e85bda299671990fa5aee052b114cd
jp_morvan <jp_morvan@ee427ae8-e902-0410-961c-c3ed070cd9f9>
2010-04-22 13:45:33 +0000

    v1.1.1

---
+++ modules/comment/templates/_comment_body.php
@@ -0,0 +1,10 @@
+
+    <tr>
+      <td class="body">
+        <?php if(!$obj->is_delete): ?>
+          <span id="body_<?php echo $obj->id ?>"><?php echo $obj->getRawValue()->getBody() ?></span>
+        <?php else: ?>
+          <span class="msg-deleted"><?php echo __('Comment deleted by moderator', array(), 'vjComment') ?></span>
+        <?php endif ?>
+      </td>
+    </tr>
