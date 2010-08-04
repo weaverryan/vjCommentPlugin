d973e88872113c869b9186c482b7da8dc125f5d2
weaverryan <ryan@thatsquality.com>
2010-08-04 18:02:32 -0500

    Just a bunch of code cleanup

--- modules/comment/templates/_comment_infos.php
+++ modules/comment/templates/_comment_infos.php
@@ -1,19 +1,23 @@
 <td rowspan="2" class="infos">
   <a name="<?php echo $i ?>" class="ancre">#<?php echo $i ?></a>
   <?php if(!$obj->is_delete): ?>
-  <?php
-            echo link_to_function(
-            image_tag('/vjCommentPlugin/images/comments.png') ,
-            "reply('".$obj->id."','".$obj->getAuthorName()."')",
-            array('title' => __('Reply to this comment', array(), 'vjComment'))) ?>
     <?php echo link_to_function(
-          image_tag('/vjCommentPlugin/images/error.png') ,
-          'window.open(
-            \''.url_for('@commentReport?id='.$obj->id.'&num='.$i).'\',
-            \''.__('Add new comment', array(), 'vjComment').'\',
-              "menubar=no, status=no, scrollbars=no, menubar=no, width=565, height=300")',
-          array('target' => '_blank', 'title' => __('Report this comment - New window', array(), 'vjComment'), 'id' => 'opener')) ?><br />
+      image_tag('/vjCommentPlugin/images/comments.png'),
+      sprintf("reply('%s','%s')", $obj->id, $obj->getAuthorName()),
+      array('title' => __('Reply to this comment', array(), 'vjComment'))
+    ) ?>
+
+    <?php echo link_to_function(
+      image_tag('/vjCommentPlugin/images/error.png'),
+      sprintf(
+        "window.open('%s','%s', 'menubar=no, status=no, scrollbars=no, menubar=no, width=565, height=300')",
+        url_for('@commentReport?id='.$obj->id.'&num='.$i),
+        __('Add new comment', array(), 'vjComment')
+      ),
+      array('target' => '_blank', 'title' => __('Report this comment - New window', array(), 'vjComment'))) ?>
+    <br />
   <?php endif; ?>
+  
   <?php if(commentTools::isGravatarAvailable() && !$obj->is_delete): ?>
     <?php echo gravatar_image_tag($obj->getAuthorEmail()) ?>
   <?php endif ?>
