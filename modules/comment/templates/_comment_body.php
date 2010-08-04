3f0b487cf3cd76bb052978f9113c9580e062d4dd
weaverryan <ryan@thatsquality.com>
2010-08-04 18:01:51 -0500

    Removing the output escaper code for two reasons:
      1) This just doesn't work if output escpaing is off
    
      2) Comments SHOULD be output escaped...that's sorta the whole point of output escaping
    
    I'm open for debate on this :)

--- modules/comment/templates/_comment_body.php
+++ modules/comment/templates/_comment_body.php
@@ -1,7 +1,7 @@
 <tr>
   <td class="body">
     <?php if(!$obj->is_delete): ?>
-      <span id="body_<?php echo $obj->id ?>"><?php echo $obj->getRawValue()->getBody() ?></span>
+      <span id="body_<?php echo $obj->id ?>"><?php echo $obj->getBody() ?></span>
     <?php else: ?>
       <span class="msg-deleted"><?php echo __('Comment deleted by moderator', array(), 'vjComment') ?></span>
     <?php endif ?>
