928ef7e3d7d39679840df7a2a871e9c4ee06c309
weaverryan <ryan@thatsquality.com>
2010-08-04 18:09:47 -0500

    Adding the ability to disable the comment reporting mechanism altogether

--- modules/comment/templates/_comment_infos.php
+++ modules/comment/templates/_comment_infos.php
@@ -7,15 +7,17 @@
       array('title' => __('Reply to this comment', array(), 'vjComment'))
     ) ?>
 
-    <?php echo link_to_function(
-      image_tag('/vjCommentPlugin/images/error.png'),
-      sprintf(
-        "window.open('%s','%s', 'menubar=no, status=no, scrollbars=no, menubar=no, width=565, height=300')",
-        url_for('@commentReport?id='.$obj->id.'&num='.$i),
-        __('Add new comment', array(), 'vjComment')
-      ),
-      array('target' => '_blank', 'title' => __('Report this comment - New window', array(), 'vjComment'))) ?>
-    <br />
+    <?php if (sfConfig::get('app_vjCommentPlugin_enable_comment_reporting')): ?>
+      <?php echo link_to_function(
+        image_tag('/vjCommentPlugin/images/error.png'),
+        sprintf(
+          "window.open('%s','%s', 'menubar=no, status=no, scrollbars=no, menubar=no, width=565, height=300')",
+          url_for('@commentReport?id='.$obj->id.'&num='.$i),
+          __('Add new comment', array(), 'vjComment')
+        ),
+        array('target' => '_blank', 'title' => __('Report this comment - New window', array(), 'vjComment'))) ?>
+      <br />
+    <?php endif; ?>
   <?php endif; ?>
   
   <?php if(commentTools::isGravatarAvailable() && !$obj->is_delete): ?>
