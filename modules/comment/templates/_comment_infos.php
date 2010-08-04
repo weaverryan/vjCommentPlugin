535046cd0f5182c211a53e49048c9c0de0303894
weaverryan <ryan@thatsquality.com>
2010-08-04 17:40:40 -0500

    code formatting

--- modules/comment/templates/_comment_infos.php
+++ modules/comment/templates/_comment_infos.php
@@ -1,22 +1,22 @@
-      <td rowspan="2" class="infos">
-        <a name="<?php echo $i ?>" class="ancre">#<?php echo $i ?></a>
-        <?php if(!$obj->is_delete): ?>
-          <?php if(''==$authorName=$obj->author_name&&sfConfig::get('app_vjCommentPlugin_guardbind')):
-                  $authorName= Doctrine::getTable('sfGuardUser')->find($obj->user_id)->getUsername();
-                  endif;
-                  echo link_to_function(
-                  image_tag('/vjCommentPlugin/images/comments.png') ,
-                  "reply('".$obj->id."','".$authorName."')",
-                  array('title' => __('Reply to this comment', array(), 'vjComment'))) ?>
-          <?php echo link_to_function(
-                image_tag('/vjCommentPlugin/images/error.png') ,
-                'window.open(
-                  \''.url_for('@commentReport?id='.$obj->id.'&num='.$i).'\',
-                  \''.__('Add new comment', array(), 'vjComment').'\',
-                    "menubar=no, status=no, scrollbars=no, menubar=no, width=565, height=300")',
-                array('target' => '_blank', 'title' => __('Report this comment - New window', array(), 'vjComment'), 'id' => 'opener')) ?><br />
-        <?php endif; ?>
-        <?php if(commentTools::isGravatarAvailable() && !$obj->is_delete): ?>
-          <?php echo gravatar_image_tag($obj->getAuthorEmail()) ?>
-        <?php endif ?>
-      </td>
+<td rowspan="2" class="infos">
+  <a name="<?php echo $i ?>" class="ancre">#<?php echo $i ?></a>
+  <?php if(!$obj->is_delete): ?>
+    <?php if(''==$authorName=$obj->author_name&&sfConfig::get('app_vjCommentPlugin_guardbind')):
+            $authorName= Doctrine::getTable('sfGuardUser')->find($obj->user_id)->getUsername();
+            endif;
+            echo link_to_function(
+            image_tag('/vjCommentPlugin/images/comments.png') ,
+            "reply('".$obj->id."','".$authorName."')",
+            array('title' => __('Reply to this comment', array(), 'vjComment'))) ?>
+    <?php echo link_to_function(
+          image_tag('/vjCommentPlugin/images/error.png') ,
+          'window.open(
+            \''.url_for('@commentReport?id='.$obj->id.'&num='.$i).'\',
+            \''.__('Add new comment', array(), 'vjComment').'\',
+              "menubar=no, status=no, scrollbars=no, menubar=no, width=565, height=300")',
+          array('target' => '_blank', 'title' => __('Report this comment - New window', array(), 'vjComment'), 'id' => 'opener')) ?><br />
+  <?php endif; ?>
+  <?php if(commentTools::isGravatarAvailable() && !$obj->is_delete): ?>
+    <?php echo gravatar_image_tag($obj->getAuthorEmail()) ?>
+  <?php endif ?>
+</td>
