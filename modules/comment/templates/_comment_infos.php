7c97d0944252fda2f564b552742d75144828eaa5
weaverryan <ryan@thatsquality.com>
2010-08-04 17:50:16 -0500

    Cleaning/fixing how we get the author name of the comment

--- modules/comment/templates/_comment_infos.php
+++ modules/comment/templates/_comment_infos.php
@@ -1,12 +1,10 @@
 <td rowspan="2" class="infos">
   <a name="<?php echo $i ?>" class="ancre">#<?php echo $i ?></a>
   <?php if(!$obj->is_delete): ?>
-    <?php if(''==$authorName=$obj->author_name&&sfConfig::get('app_vjCommentPlugin_guardbind')):
-            $authorName= Doctrine::getTable('sfGuardUser')->find($obj->user_id)->getUsername();
-            endif;
+  <?php
             echo link_to_function(
             image_tag('/vjCommentPlugin/images/comments.png') ,
-            "reply('".$obj->id."','".$authorName."')",
+            "reply('".$obj->id."','".$obj->getAuthorName()."')",
             array('title' => __('Reply to this comment', array(), 'vjComment'))) ?>
     <?php echo link_to_function(
           image_tag('/vjCommentPlugin/images/error.png') ,
