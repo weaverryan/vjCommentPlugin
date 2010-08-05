6ba9845f7c3865bca1198c921957759ffe483a80
Ryan Weaver <ryan@thatsquality.com>
2010-08-05 06:43:53 -0500

    Small code cleanup in templates

--- modules/comment/templates/_commentlist.php
+++ modules/comment/templates/_commentlist.php
@@ -1,7 +1,9 @@
-  <?php use_helper('Date', 'JavascriptBase', 'I18N') ?>
-<div><h1><?php echo __('Comments list', array(), 'vjComment') ?></h1></div>
-  <table class="list-comments" summary="">
+<?php use_helper('Date', 'JavascriptBase', 'I18N') ?>
+
+<h1><?php echo __('Comments list', array(), 'vjComment') ?></h1>
+
+<table class="list-comments" summary="">
   <?php foreach($comments as $c): ?>
     <?php include_partial("comment/comment", array('obj' => $c, 'i' => ++$i)) ?>
   <?php endforeach; ?>
-  </table>
+</table>
--- modules/commentReportAdmin/templates/_comment_link.php
+++ modules/commentReportAdmin/templates/_comment_link.php
@@ -1,5 +1,6 @@
 <?php use_helper('JavascriptBase') ?>
 <?php use_stylesheet("/vjCommentPlugin/css/commentReportAdmin.css", 'last') ?>
 <?php echo link_to_function(
-              image_tag('/vjCommentPlugin/images/magnifier.png', array('alt' => '')) ,
-              "window.open('".$comment_report->getReferer()."', 'Reporter un commentaire', 'menubar=no, status=no, scrollbars=yes, menubar=no, width=565, height=600')", array('target' => '_blank')) ?>
+  image_tag('/vjCommentPlugin/images/magnifier.png', array('alt' => '')) ,
+  "window.open('".$comment_report->getReferer()."', 'Reporter un commentaire', 'menubar=no, status=no, scrollbars=yes, menubar=no, width=565, height=600')", array('target' => '_blank'))
+?>
