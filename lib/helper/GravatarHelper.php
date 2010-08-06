67a413f3029b2b68a9f72df023abafc6cea21015
Ryan Weaver <ryan@thatsquality.com>
2010-08-05 22:00:00 -0500

    Adding a gravatar_image_path which makes a little more sense than the other method and allows you more control in your image tag

--- lib/helper/GravatarHelper.php
+++ lib/helper/GravatarHelper.php
@@ -11,12 +11,25 @@
  */
 function gravatar_image_tag($email, $gravatar_rating = null, $gravatar_size = null, $alt_text = 'Gravatar photo')
 {
-  $gravatar = new GravatarApi($gravatar_rating, $gravatar_size);
-
-  return image_tag($gravatar->getGravatar($email), array(
+  return image_tag(gravatar_image_path($email, $gravatar_size, $gravatar_rating), array(
     'alt' => $alt_text,
     'width' => sfConfig::get('app_gravatar_default_size', 80),
     'height' => sfConfig::get('app_gravatar_default_size', 80),
     'class' => 'gravatar_photo'
   ));
+}
+
+/**
+ * Returns the image path to a gravatar image
+ *
+ * @param  string  $email  The email address for the gravatar
+ * @param  integer $size  The size of the image to be downloaded and cached
+ * @param  string $rating  The rating of the image to fetch
+ * @return string
+ */
+function gravatar_image_path($email, $size = null, $rating = null)
+{
+  $gravatar = new GravatarApi($size, $rating);
+
+  return $gravatar->getGravatar($email);
 }
