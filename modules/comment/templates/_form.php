4a333545a4f0bacc9a81fb9db41d57bb693143d4
weaverryan <ryan@thatsquality.com>
2010-10-25 12:08:20 -0500

    Cleaning up how all the markup is output - it's still a bit of a mess, but better.

--- modules/comment/templates/_form.php
+++ modules/comment/templates/_form.php
@@ -1,32 +1,38 @@
 <table summary="">
-<?php foreach($form as $id => $f): ?>
-  <?php if($id == "reply_author" && $f->getValue()!= ""): ?>
-    <?php use_stylesheet(sfConfig::get('app_vjCommentPlugin_style_web_root').'/css/replyTo.css', "last") ?>
-  <?php endif ?>
-  <?php if(!$f->isHidden()): ?>
-    <?php if(!$f->hasError()): ?>
-      <?php $attributes = array() ?>
-    <?php else: ?>
-      <?php $attributes = array('class' => 'error') ?>
-      <tr>
-        <td></td>
-        <td><?php echo $f->renderError() ?></td>
+  <?php foreach($form as $id => $f): ?>
+    <?php $attributes = array() ?>
+
+    <?php if($id == "reply_author" && $f->getValue()!= ""): ?>
+      <?php use_stylesheet(sfConfig::get('app_vjCommentPlugin_style_web_root').'/css/replyTo.css', "last") ?>
+    <?php endif ?>
+
+    <?php if(!$f->isHidden()): ?>
+      <?php if($f->hasError()): ?>
+        <?php $attributes['class'] = 'error' ?>
+        <tr>
+          <td></td>
+          <td><?php echo $f->renderError() ?></td>
+        </tr>
+      <?php endif ?>
+
+      <tr id="tr_<?php echo $id ?>">
+        <th>
+          <?php echo $f->renderLabel(null, $attributes) ?>
+        </th>
+        <td>
+          <?php echo $f->render($attributes) ?>
+          <span class="help"><?php echo $f->renderHelp() ?></span>
+        </td>
       </tr>
+
+      <?php if($id == "reply_author"): ?>
+        <tr id="tr_reply_author_delete">
+          <td colspan="2"><?php echo link_to_function(__("Delete the reply", array(), 'vjComment'), "deleteReply()", array('class' => 'delete_reply'))."\n" ?></td>
+        </tr>
+      <?php endif ?>
+      
     <?php endif ?>
-    <tr id="tr_<?php echo $id ?>">
-      <th>
-        <?php echo $f->renderLabel(null, $attributes) ?>
-      </th>
-      <td>
-        <?php echo $f->render($attributes) ?>
-        <span class="help"><?php echo $f->renderHelp() ?></span>
-      </td>
-    </tr>
-    <?php if($id == "reply_author"): ?>
-    <tr id="tr_reply_author_delete">
-      <td colspan="2"><?php echo link_to_function(__("Delete the reply", array(), 'vjComment'), "deleteReply()", array('class' => 'delete_reply'))."\n" ?></td>
-    </tr>
-    <?php endif ?>
-  <?php endif ?>
-<?php endforeach ?>
+
+  <?php endforeach ?>
+</table>
 <?php echo $form->renderHiddenFields() ?>
